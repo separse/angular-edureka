d53ba54b55309e136c8ade3ce68e7fd04fe03fde
separse <sepehrroshanpour@gmail.com>
2021-01-03 12:25:19 +0330

    ngContent-contentChild

--- src/app/header/view-child/inner-view-child/inner-view-child.component.ts
+++ src/app/header/view-child/inner-view-child/inner-view-child.component.ts
@@ -1,13 +1,25 @@
-import { Component } from "@angular/core";
+import { AfterContentChecked, AfterContentInit, Component, ContentChild, ElementRef, OnInit } from "@angular/core";
 
 @Component({
     selector: 'app-inner-view-child',
     templateUrl: './inner-view-child.component.html'
 })
 
-export class InnerViewChildComponent {
+export class InnerViewChildComponent implements OnInit, AfterContentInit, AfterContentChecked {
+    @ContentChild('par', {static: true, read: ElementRef}) par!: ElementRef;
     num: number = 0;
     plusOne() {
         this.num++;
     }
+    
+    ngOnInit(): void {
+        console.log('oninit' + this.par.nativeElement.textContent);
+    }
+    ngAfterContentInit(): void {
+        console.log('afterinit' + this.par.nativeElement.textContent);
+    }
+    ngAfterContentChecked(): void {
+        console.log('aftercheked' + this.par.nativeElement.textContent);
+    }
+    
 }
--- src/app/header/view-child/view-child.component.ts
+++ src/app/header/view-child/view-child.component.ts
@@ -16,26 +16,32 @@ import { InnerViewChildComponent } from "./inner-view-child/inner-view-child.com
     encapsulation: ViewEncapsulation.None
 })
 
-export class ViewChildComponent implements AfterViewInit {
+export class ViewChildComponent implements  OnInit {
 
     // @ViewChild(InnerViewChildComponent, { static: true, read: InnerViewChildComponent }) innerChild!: InnerViewChildComponent;
     // @ViewChild('num2', { static: true, read: ElementRef }) num2!: ElementRef;
+    @ViewChild('par', {static: true, read: ElementRef}) par!: ElementRef;
     // @ViewChild('num2', {static: true}) num2!: ElementRef; المنت رف را از اینجا تغییر ندهیم فقط میتوانیم بخانیم
     @ViewChildren(InnerViewChildComponent, { read: InnerViewChildComponent }) innerChildren!: QueryList<InnerViewChildComponent>;
 
     constructor(private renderer: Renderer2) { }
 
-    // ngOnInit(): void {
-    //     this.num2.nativeElement.innerHTML = 'efwfw';
-    //     this.renderer.setStyle(this.num2.nativeElement, 'color', 'blue');
-    //     this.innerChild.num = 10;
-    // }
+    ngOnInit(): void {
+        this.renderer.setStyle(this.par.nativeElement, 'background', 'blue');
+        // this.num2.nativeElement.innerHTML = 'efwfw';
+        // this.renderer.setStyle(this.num2.nativeElement, 'color', 'blue');
+        // this.innerChild.num = 10;
+    }
     // تا وقتی که میشود از افتر استفاده شود
     // ngAfterViewInit(): void { 
     //     this.num2.nativeElement.innerHTML = 'efwfw';
     // }
 
-    ngAfterViewInit(): void {
-      this.innerChildren.forEach((comp) => comp.plusOne());
+    // ngAfterViewInit(): void {
+    //   this.innerChildren.forEach((comp) => comp.plusOne());
+    // }
+
+    public changePar () {
+        this.par.nativeElement.textContent = 'googooli';
     }
 }
