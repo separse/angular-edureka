cdaaeeec2c7cbf26e4797aa6bb5220714af7fdc2
separse <sepehrroshanpour@gmail.com>
2021-01-03 13:45:50 +0330

    ng-container

--- src/app/header/users/users.component.ts
+++ src/app/header/users/users.component.ts
@@ -3,12 +3,9 @@ import { User } from "./user-interface";
 
 @Component({
     selector: 'app-users',
-    template: `
-        <app-user [user]="user1"></app-user>
-        <app-users-form (pushUser)="pushUser($event)"></app-users-form>
-        <app-users-table [users]="users"></app-users-table>
-    `
+    templateUrl: './users.component.html'
 })
+
 export class UsersComponent {
     user1: User =
         {
@@ -16,27 +13,49 @@ export class UsersComponent {
             email: 'sepehrar@yahoo.com',
             pass: '09180189098'
         };
+    users1 = [
+        {
+            name: 'Russia',
+            email: 'sepehr@yahoo.com',
+            pass: 34
+        },
+        {
+            name: 'Olson',
+            email: 'warning@gmail.com',
+            pass: 24
+        },
+        {
+            name: 'Gooria',
+            email: 'gogooli@yahoo.com',
+            pass: 44
+        },
+        {
+            name: 'Hella',
+            email: 'sorus@yahoo.com',
+            pass: 11
+        }
+    ];
 
     users: User[] = [
         {
             name: 'Russia',
             email: 'sepehr@yahoo.com',
-            pass: '09180186198'
+            pass: '34'
         },
         {
             name: 'Olson',
             email: 'warning@gmail.com',
-            pass: '09357930901'
+            pass: '24'
         },
         {
             name: 'Gooria',
             email: 'gogooli@yahoo.com',
-            pass: '09123457612'
+            pass: '44'
         },
         {
             name: 'Hella',
             email: 'sorus@yahoo.com',
-            pass: '09378764501'
+            pass: '11'
         }
     ];
 
