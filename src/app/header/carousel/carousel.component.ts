3b622d3234dacf9d94b633db75079d6c8bab09aa
separse <sepehrroshanpour@gmail.com>
2021-01-04 14:31:07 +0330

    Structural Directive

--- src/app/header/carousel/carousel.component.ts
+++ src/app/header/carousel/carousel.component.ts
@@ -1,10 +1,15 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-carousel',
-    templateUrl: './carousel.component.html'
+  selector: 'app-carousel',
+  templateUrl: './carousel.component.html',
 })
-
 export class CarouselComponent {
-    backgroundColor = "red"
+  backgroundColor = 'red';
+  images = [
+    '/assets/Join-Leaders-Alt1.jpg',
+    '/assets/Join-Leaders-Alt5.jpg',
+    '/assets/Join-Leaders-Alt3.jpg',
+    '/assets/Join-Leaders-Alt4.jpg',
+  ];
 }
--- src/app/header/carousel/carousel.directive.ts
+++ src/app/header/carousel/carousel.directive.ts
@@ -1,10 +1,66 @@
-import { Directive } from '@angular/core';
+import {
+  Directive,
+  Input,
+  OnInit,
+  TemplateRef,
+  ViewContainerRef,
+} from '@angular/core';
+import { ICarouselContext } from './carousel-contex';
 
 @Directive({
-  selector: '[appCarousel]'
+  selector: '[appCarousel]',
 })
-export class CarouselDirective {
+export class CarouselDirective implements OnInit {
+  context!: ICarouselContext;
+  index = 0;
+  timer!: any;
 
-  constructor() { }
+  @Input('appCarouselFrom') images!: string[];
+  @Input('appCarouselAutoPlay')
+  set autoPlay(val: string) {
+    val === 'No' ? this.stopAutoPlay() : this.runAutoPlay();
+  }
 
+  constructor(
+    private templateRef: TemplateRef<ICarouselContext>,
+    private viewContainerRef: ViewContainerRef
+  ) {}
+
+  ngOnInit(): void {
+    this.context = {
+      $implicit: this.images[0],
+      controller: {
+        next: () => this.next(),
+        previous: () => this.previous(),
+      },
+    };
+
+    this.viewContainerRef.createEmbeddedView(this.templateRef, this.context);
+  }
+
+  public next() {
+    this.index++;
+    if (this.index >= this.images.length) {
+      this.index = 0;
+    }
+    this.context.$implicit = this.images[this.index];
+  }
+
+  public previous() {
+    this.index--;
+    if (this.index < 0) {
+      this.index = this.images.length - 1;
+    }
+    this.context.$implicit = this.images[this.index];
+  }
+
+  public runAutoPlay() {
+    this.timer = setInterval(() => {
+      this.next();
+    }, 2000);
+  }
+
+  public stopAutoPlay() {
+    clearInterval(this.timer);
+  }
 }
